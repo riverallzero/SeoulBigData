4721d076f98f58b9e872432b15751b3f2d19286a
riverallzero <kallzero1008@jbnu.ac.kr>
2023-03-10 13:11:34 +0900

    folium map

--- Dayoung/density.py
+++ Dayoung/density.py
@@ -15,6 +15,7 @@ df.columns = ["name", "values"]
 # json파일과 동이름 일치하도록 변경
 df["name"] = df["name"].apply(lambda x: x.replace("제", "") if "제" in x else x)
 dong_list = df["name"][1:].values
+df = df[1:].reset_index(drop=True)
 print(f"강서구 총 동의 개수:{len(dong_list)} \n {dong_list}")
 
 code_data = []
@@ -23,35 +24,36 @@ with open("Data/Gangseogu_geo.json", "w", encoding="utf-8-sig") as f:
     geo_data = []
     for feature in data["features"]:
         if feature["properties"]["name"] in dong_list:
+            code = feature["properties"]["code"]
+            if str(code).startswith("2"):
+                continue
             geo_data.append(feature)
             code_data.append({"dong": feature["properties"]["name"], "code": feature["properties"]["code"]})
     json.dump(geo_data, f, ensure_ascii=False, indent="\t")
 
 dong_data = pd.DataFrame.from_dict(code_data)
-dong_data = dong_data.sort_values("dong")
-dong_data = dong_data[~dong_data['code'].astype(str).str.startswith('2')].reset_index(drop=True)
-df = df[1:].sort_values("name").reset_index(drop=True)
+dong_data = dong_data.sort_values("dong").reset_index(drop=True)
+df = df.sort_values("name").reset_index(drop=True)
 
 # 사용할 동 데이터
-df_dong = pd.concat([dong_data, df["values"]], axis=1)
+df_dong = pd.concat([df, dong_data["code"]], axis=1).reset_index(drop=True)
 print(df_dong)
 
 print("-"*10, "[Done] 강서구 데이터 추출 완료")
 
-
 # Folium 데이터 그리기
 geo_str = json.load(open("Data/Gangseogu_geo.json", encoding="utf-8-sig"))
-seoul_map = folium.Map(location=[37.5502, 126.982], zoom_start=10.5, titles="cartodbpositron")
-seoul_map.choropleth(
+m = folium.Map(
+    location=[37.5502, 126.982],
+    zoom_start=12,
+)
+m.choropleth(
     geo_data=geo_str,
     data=df_dong,
     columns=["code", "values"],
-    fill_color="PuRd",
-    key_on="properties.code",
-    highlight=True,
-    fill_opacity=0.5,
-    line_opacity=1,
-    legend_name="population(people)"
-                     )
-seoul_map.save("Gangseogu_map.html")
-print(seoul_map)
+    fill_color="YlGnBu",
+    fill_opacity=0.7,
+    line_opacity=0.5,
+    legend_name="population(people)",
+)
+print(m)
